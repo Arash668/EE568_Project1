97a7d3b23822253340193d94845977849232cb11
Arash <84270922+Arash668@users.noreply.github.com>
2022-04-11 14:51:45 +0300

    Update code1_analytical.m

--- code1_analytical.m
+++ code1_analytical.m
@@ -1,6 +1,6 @@
 Lstack =20e-3;
-Airgap_min = 0.5e-3;
-Airgap_max = 2.5e-3;
+Airgap_min = 2*0.5e-3;
+Airgap_max = 2*2.5e-3;
 u0 = pi*4e-7;
 I_dc = 3 ; 
 Nseries = 250; % number of turns
@@ -11,22 +11,20 @@ Stator_radius = 12e-3;
 
 % when rotor position is 90 deg minimum reluctance is achieved 
 
-Rmin =  Airgap_min/(u0*Stator_radius*Stator_Pole_arc*Lstack);
+Area = Stator_radius*Stator_Pole_arc*Lstack;
+Rmin =  Airgap_min/(u0*Area);
 % when rotor position is 0 deg maximum reluctance is achieved
-Rmax = Airgap_max/(u0*Stator_radius*Stator_Pole_arc*Lstack);
+Rmax = Airgap_max/(u0*Area);
 
 theta = 0:360;
-% I consider a sinusoidal variatoin for the reluctance
-% there are two airgap, here I just calculated the torque for airgap1 and
-% at the end we multiply the torque by 2 to 
-R_airgap = (Rmax+Rmin)/2+(Rmax-Rmin)*cosd(2*theta)/2;
 
-L = Nseries^2./R_airgap;
-% drivative of the inductance L has been taken by hand and below is the
-% resutltant dl/dtheta
-DL_Dtheta = -1*(Nseries^2)*((Rmax-Rmin)/2*(-2*sind(2*theta)))./(R_airgap.^2);
+Lmax = Nseries^2/Rmin;
+Lmin = Nseries^2/Rmax;
+LL = (Lmax+Lmin)/2+(Lmax-Lmeeein)*cosd(2*theta)/2;
 
-Torque = 2*(I_dc^2*DL_Dtheta)/2;
+DL_Dtheta = -2*(Lmax-Lmin)*sind(2*theta)/2;
+
+Torque = (I_dc^2*DL_Dtheta)/2;
 
 figure
 plot(theta,R_airgap)
